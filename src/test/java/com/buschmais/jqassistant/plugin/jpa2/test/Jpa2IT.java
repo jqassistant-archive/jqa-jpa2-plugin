8b29d94755e19965bdd02889d9f1f2fe284abb6a
Dirk Mahler <dirk.mahler@buschmais.com>
2022-03-26 16:40:13 +0100

    moved matchers from core.store and core.analysis to core.test

--- src/test/java/com/buschmais/jqassistant/plugin/jpa2/test/Jpa2IT.java
+++ src/test/java/com/buschmais/jqassistant/plugin/jpa2/test/Jpa2IT.java
@@ -21,10 +21,10 @@ import com.buschmais.jqassistant.plugin.jpa2.test.set.entity.SingleNamedQueryEnt
 import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
 
-import static com.buschmais.jqassistant.core.analysis.test.matcher.ConstraintMatcher.constraint;
-import static com.buschmais.jqassistant.core.analysis.test.matcher.ResultMatcher.result;
 import static com.buschmais.jqassistant.core.report.api.model.Result.Status.FAILURE;
 import static com.buschmais.jqassistant.core.report.api.model.Result.Status.SUCCESS;
+import static com.buschmais.jqassistant.core.test.matcher.ConstraintMatcher.constraint;
+import static com.buschmais.jqassistant.core.test.matcher.ResultMatcher.result;
 import static com.buschmais.jqassistant.plugin.java.test.matcher.FieldDescriptorMatcher.fieldDescriptor;
 import static com.buschmais.jqassistant.plugin.java.test.matcher.MethodDescriptorMatcher.methodDescriptor;
 import static com.buschmais.jqassistant.plugin.java.test.matcher.TypeDescriptorMatcher.typeDescriptor;
